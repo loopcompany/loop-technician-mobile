705a2ab554018bcea90a935c5ac1703729e66835
unknown <nilofarsafary>
2025-11-11 11:22:17 +0330

    pull from honey3

--- services/URL.js
+++ services/URL.js
@@ -1,10 +1,6 @@
 export const mainUri = 'https://khayyamtech.com';
 export const uri = 'https://khayyamtech.com/api';
-<<<<<<< HEAD
-export const imageUri = 'http://192.168.21.123:8000/storage';
-=======
 export const imageUri = 'https://khayyamtech.com/storage';
->>>>>>> 0962dff4effe3b6987c1be5e6378b3fcee234040
 
 // Technician API URLs
 export const Technician_Orders = `${uri}/technician/orders`;
